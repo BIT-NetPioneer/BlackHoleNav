9af45d9e437c64e2096924392cef6d2770d99729
HacRi <HacRi@HacRi-PC>
2012-08-29 13:55:11 +0800

    Update: 更新方法和文件的说明注释

--- application/models/config_m.php
+++ application/models/config_m.php
@@ -18,6 +18,13 @@ class Config_m extends CI_Model {
         parent::__construct();
     }
 
+    /**
+     * 获取数据库中保存的config
+     * 
+     * 获取数据库中保存的config值，如果数据库中不存在该值，则自动新建，并返回空值。
+     * @param string $item config名称
+     * @return string 结果
+     */
     function get_config($item) {
         $this->db->where('item', $item);
         $this->db->select('value');
@@ -33,6 +40,12 @@ class Config_m extends CI_Model {
         }
     }
 
+    /**
+     * 保存config值
+     * 
+     * @param string $item config值的名称，数据库表中必须已经存在该值
+     * @param string $value config值的内容
+     */
     function set_config($item, $value) {
         $this->db->where('item', $item);
         $data = array('value' => $value);
